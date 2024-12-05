4c073a952a0897b6b5eb2680aaf2617905470df6
igorqa <igorqa@nous.dev>
2024-12-05 21:22:09 +0500

    Обновление теста: Добавлен переход на страницу ввода проверочного кода

--- app_test.py
+++ app_test.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
@@ -34,15 +35,30 @@ class TestAppium(unittest.TestCase):
         if self.driver:
             self.driver.quit()
 
-    def test_registartion_flow(self) -> None:
-        xpath_phone_field = '//android.widget.EditText'
+# Авторизация по номеру телефона
+    def test_auth_by_phone_flow(self) -> None:
+        # Объявление локаторов и тестовых данных
+        xpath_phone_field = 'android.widget.EditText'
+        phoneNumber = 9530058635
+        approve_text = "Введите последние 4 цифры номера, с которого поступил звонок, или код из СМС"
         button_xpath = '//android.widget.Button[@content-desc="Далее"]'
 
+        # Получение текстового поля для ввода номера телефона
+        el1 = self.driver.find_element(by=AppiumBy.CLASS_NAME, value=xpath_phone_field)
+        # Клик по полю ввода номера телефона
+        el1.click(phoneNumber)
+        time.sleep(2)
+        # Вввод номера телефона
+        el1.send_keys()
+
+        # Получение кнопки Далее
         button_send = self.driver.find_element(by=AppiumBy.XPATH, value=button_xpath)
+        # Нажатие не кнопку Далее
         button_send.click()
 
-        error_text = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[@content-desc="Неправильный номер"]'))
+        # Текст подтверждающий что открылась страница ввода проверочного кода открылась
+        approve_el = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.XPATH, f'//android.view.View[@content-desc="{approve_text}"]'))
         )
 
 
